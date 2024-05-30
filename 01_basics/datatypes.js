8954f7804feb0a8a92472abe27009782be7b9bb4
itsavi8433 <hindolabhishek8433@gmail.com>
2024-05-30 13:36:15 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+let  age =18;
+let  isLoggedIn = true;
+let state = null;
+let city;
+
+//there are 7 primitive data types in javascript
+
+// number
+//string
+//bigint
+//boolean
+//null   ---> stand alone value
+//undefined  ----> (value not assign)(khali)
+//symbol  ----> indentifed the unique component
+
+//non-primitive data types
+
+//object
+
+console.log(typeof "abhishek")
+console.log(typeof age);
+console.log(typeof null);  //null is a object datatype
+console.log(typeof undefined);  //undifined is a undifined datatypes
