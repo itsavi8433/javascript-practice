55e72e2d2641391923b6899964d77c0f21cf6c69
itsavi8433 <hindolabhishek8433@gmail.com>
2024-05-30 13:03:26 +0000

    conatiner config done

---
+++ 01_basics/let-var-const.js
@@ -0,0 +1,23 @@
+const accountId = 8433;
+let accountname ="Abhishek singh"
+var accountEmail = "hindolabhishek8433@gmail.com"
+accountCity = "Mathura"
+
+// accountId = 8126;
+
+/*
+1.prefer not use var in any case because it will be problamatic in functional and block spaces
+2.const variable can not be reassign in any case
+3.let variable can not be reassign but we updated the value after assign the value
+4.var variable can be reassign or updated after the assign value.
+*/
+
+
+accountName = "sumit singh"
+accountEmail = "singhsumit8126@gmail.com"
+accountCity ="noida"
+let accountState;
+
+
+
+console.table([accountName, accountId, accountEmail, accountCity, accountState]);
