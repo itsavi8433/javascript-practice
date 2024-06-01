3f04a744f6f47eb165e7c57ee17549051a679493
itsavi8433 <hindolabhishek8433@gmail.com>
2024-06-01 03:48:25 +0000

    practice on conversion topic

---
+++ 01_basics/practice.js
@@ -0,0 +1,46 @@
+// // let score = 33;
+// // console.log(typeof score);
+// // viewInString = String(score);
+// // console.log(typeof viewInString);     //type ==>string
+// // console.log(viewInString);            //value ==>33
+
+// // //type will be a string but the value will be a  number 
+
+// // let finish = "33abc";
+// // console.log(typeof finish);         //type ==>string
+// // viewInNumber = Number(finish);       
+// // console.log(typeof viewInNumber);    //type ==>Number
+// // console.log(viewInNumber);           //value ==> Not a Number(NaN)
+
+// // let answer = true;
+// // console.log(typeof answer);        //type ==>Boolean
+// // viewInNum = Number(answer);         
+// // console.log(typeof viewInNum);     //type ==>Number
+// // console.log(viewInNum);            //value ==>1
+
+// // let ans = false;
+// // console.log(typeof ans);
+// // viewinNumerical = Number(ans);
+// // console.log(typeof viewinNumerical);
+// // console.log(viewinNumerical);
+
+// let answer = 0;
+// console.log(typeof answer);            //type ==>Number
+// viewInBoolean = Boolean(answer);
+// console.log(typeof viewInBoolean);     //type ==>boolean
+// console.log(viewInBoolean);             //value ==>1---true    0---false
+
+// answer = "abhishek"
+// console.log(typeof answer);             //type ==>string
+// viewinboolean = Boolean(answer);         
+// console.log(typeof viewinboolean);      //type ==>boolean
+// console.log(viewinboolean);            //value ==>true
+
+let score = undefined;
+console.log(typeof score);
+viewInNumber = Number(score);
+console.log(typeof viewInNumber);
+console.log(viewInNumber);
+
+//Null is a stand alone value that's why null convert into a number and it gives a value 0
+//but undefined is vacant space that's why undefined convert into a number and it gives a value (NaN)(Not a Number)
